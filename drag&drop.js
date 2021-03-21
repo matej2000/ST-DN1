acfd0f29837a0f519a57dafbe9b4e89b075ac559
Matej <matejvatovec22@gmail.com>
2021-03-21 23:24:44 +0100

    D&d, rocno izberi sliko

--- drag&drop.js
+++ drag&drop.js
@@ -7,10 +7,23 @@ function dropHandler(ev){
     if(ev.dataTransfer.items){
         for(var i = 0; i < ev.dataTransfer.items.length; i++){
             if(ev.dataTransfer.items[i].kind == 'file'){
-                var file = ev.dataTransfer.items[i].getData();
-                document.getElementById("drop_zone").style.backgroundImage = file;
-                //document.getElementById("drop_zone").style.backgroundColor = "red";
-                console.log('... file[' + i + '].name = ' + file.name);
+                var file = ev.dataTransfer.items[i].getAsFile();
+                if(file.type.split("/")[0].localeCompare("image") == 0){
+                    var fr = new FileReader();
+                    var preview = document.querySelector('img');
+                   
+                    fr.onload = function(e){
+                        console.log(e.target.result);
+                    }
+                    //fr.readAsDataURL(file);
+                    fr.addEventListener("load",function(){
+                        preview.src = fr.result;
+                    }, false)
+                    fr.readAsDataURL(file);
+                    //document.getElementById("drop_zone").style.backgroundImage = "url('"+ file.name +"')";
+                    //document.getElementById("drop_zone").style.backgroundColor = "red";
+                    console.log('... file[' + i + '].name = ' + file.name + ", type = " + file.type + ", src = " + file.src);
+                }
             }
         }
     }
@@ -27,3 +40,18 @@ function dragOverHandler(ev) {
     // Prevent default behavior (Prevent file from being opened)
     ev.preventDefault();
   }
+
+function previewFile(){
+    const preview = document.querySelector('img');
+    const file = document.querySelector('input[type=file]').files[0];
+    const reader = new FileReader();
+
+    reader.addEventListener("load", function () {
+        // convert image file to base64 string
+        preview.src = reader.result;
+    }, false);
+
+    if (file) {
+        reader.readAsDataURL(file);
+    }
+}
