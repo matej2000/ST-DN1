1e28291353c7028f2706e1e613e273c36c10e721
Matej <matejvatovec22@gmail.com>
2021-03-21 18:08:13 +0100

    Drag&drop test

--- drag&drop.js
+++ drag&drop.js
@@ -7,8 +7,10 @@ function dropHandler(ev){
     if(ev.dataTransfer.items){
         for(var i = 0; i < ev.dataTransfer.items.length; i++){
             if(ev.dataTransfer.items[i].kind == 'file'){
-                var file = ev.dataTransfer.items[i].getAsFile();
-                console.log('... file[' + i + '].name = ' + file.name)
+                var file = ev.dataTransfer.items[i].getData();
+                document.getElementById("drop_zone").style.backgroundImage = file;
+                //document.getElementById("drop_zone").style.backgroundColor = "red";
+                console.log('... file[' + i + '].name = ' + file.name);
             }
         }
     }
