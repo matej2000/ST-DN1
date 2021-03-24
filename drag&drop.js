4d9fa736414636c575d077fda337081b937db804
Matej <matejvatovec22@gmail.com>
2021-03-24 16:04:08 +0100

    Odstranu bootstrap, izbris slik

--- drag&drop.js
+++ drag&drop.js
@@ -1,5 +1,6 @@
 const loaded_files = new Array();
 const img_tags = new Array();
+const first = true;
 // TODO: prikazovanje več slik(zanka for skozi loaded_files), zvezdica k sliki, opis k sliki, izbirsi sliko, prilagodi za različne naprave
 // FIXME: popravi premikanje slik, popravi bug infinit loop ko prehajas cez sliko.
 function dropHandler(ev){
@@ -14,7 +15,20 @@ function dropHandler(ev){
                 var file = ev.dataTransfer.items[i].getAsFile();
                 if(file.type.split("/")[0].localeCompare("image") == 0){
                     var fr = new FileReader();
-                    var preview = document.querySelector('img');
+                    //var preview = document.querySelector('img');
+                    var preview = new Image();
+                    var divOutside = document.createElement("div");
+                    var input = document.createElement("input");
+                    
+                    divOutside.style.display = "inline-block";
+                    divOutside.style.textAlign = "center";
+                    divOutside.appendChild(preview);
+                    divOutside.appendChild(input);
+                    
+                    divOutside.addEventListener('dblclick', function (e) {
+                        console.log("Double click");
+                        document.getElementById("drop_zone").removeChild(this);
+                      });
                    
                     /*fr.onload = function(e){
                         console.log(e.target.result);
@@ -24,7 +38,8 @@ function dropHandler(ev){
                         document.getElementById("dzP").innerHTML = "";
                         preview.src = fr.result;
                         preview.style.display = "";
-                        document.getElementById("drop_zone").style.border = "";
+                        document.getElementById("drop_zone").appendChild(divOutside);
+                        document.getElementById("drop_zone").style.outline= "";
                     }, false)
                     fr.readAsDataURL(file);
                     //document.getElementById("drop_zone").style.backgroundImage = "url('"+ file.name +"')";
@@ -32,7 +47,7 @@ function dropHandler(ev){
                 }
             }
             else{
-                document.getElementById("drop_zone").style.border = "";
+                document.getElementById("drop_zone").style.outline = "";
                 document.getElementById("dzP").innerHTML = "";
                 //document.getElementById("drop_zone").innerHTML = "Ups something went wrong with loading your file"
             }
@@ -48,7 +63,7 @@ function dropHandler(ev){
 function dragOverHandler(ev) {
     console.log('File(s) in drop zone');
     var drop_zone = document.getElementById("drop_zone");
-    drop_zone.style.border = "5px dashed grey"
+    drop_zone.style.outline = "5px dashed grey"
     var dzp = document.getElementById("dzP");
     dzp.innerHTML = "Drag one or more images to this drop zone"
 
@@ -75,17 +90,32 @@ function previewFile(){
 }
 
 function dragLeaveHandler(ev){
-    document.getElementById("drop_zone").style.border = "";
+    document.getElementById("drop_zone").style.outline = "";
     document.getElementById("dzP").innerHTML = "";
     console.log("File left");
 }
 
 function dragEnterHandler(ev){
-    document.getElementById("drop_zone").style.border = "";
+    document.getElementById("drop_zone").style.outline = "";
     document.getElementById("dzP").innerHTML = "";
     console.log("File entered");
 }
 
+
 /*function dragOverHandler(ev){
     console.log("File over");
 }*/
+
+
+
+
+/*<!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+
+    <!-- jQuery library -->
+   
+    <!-- Popper JS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+
+    <!-- Latest compiled JavaScript onerror="this.style.display='none'"-->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>*/
